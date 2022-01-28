d813705dcf5711278056c088635fc2410be03843
Kyle <kylecindric@gmail.com>
2022-01-28 14:43:34 -0600

    Created computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // Rock Paper Scissors Script
+
+function computerPlay() {
+    switch(Math.floor(Math.random() * 3)) {
+        case 0:
+            console.log("Computer chose rock")
+            break;
+        case 1:
+            console.log("Computer chose paper")
+            break;
+        case 2:
+            console.log("Computer chose scissors")
+            break;
+        default:
+            console.log("wut?")
+    }
+}
+
+computerPlay()
+// const choice = computerPlay()
+
+// switch(choice) {
+//     case 0:
+//         console.log("Computer chose rock")
+//         break;
+//     case 1:
+//         console.log("Computer chose paper")
+//         break;
+//     case 2:
+//         console.log("Computer chose scissors")
+//         break;
+//     default:
+//         console.log("wut?")
+// }
+
